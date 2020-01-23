9378d4511a9475dbdbc01ce6a51caa6e0070bdcf
Teodor Hristov <teodor214@gmail.com>
2020-01-23 17:58:13 +0200

    Add files via upload

---
+++ 08. Triangle of Numbers/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace _08._Triangle_of_Numbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num = int.Parse(Console.ReadLine());
+
+            for (int row = 1; row <= num; row++)
+            {
+                for (int cols = 1; cols <= row; cols++)
+                {
+                    Console.Write($"{row} ");
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
