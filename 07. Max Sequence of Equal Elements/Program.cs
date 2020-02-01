9596d62f8b883cf761f2c485032ba0716ad5a5b3
Teodor Hristov <teodor214@gmail.com>
2020-02-01 16:31:32 +0200

    Add files via upload

---
+++ 07. Max Sequence of Equal Elements/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Linq;
+
+namespace _07._Max_Sequence_of_Equal_Elements
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers = Console.ReadLine()
+                 .Split()
+                 .Select(int.Parse)
+                 .ToArray();
+
+            int count = 0;
+            
+            int maxCount = 0;
+            int oldElement = numbers[0];
+            int newElement = 0;
+            int numToPrint = numbers[0];
+
+
+            for (int i = 0; i < numbers.Length-1; i++)
+            {
+                oldElement = numbers[i];
+                newElement = numbers[i + 1];
+                if (newElement == oldElement)
+                {
+                   
+                    count++;
+
+                    if (count>maxCount)
+                    {
+                        maxCount = count;
+                        numToPrint = oldElement;
+
+                    }
+                }
+
+                else
+                {
+                    count = 0;
+
+                }
+            }
+
+            for (int j = 0; j <= maxCount; j++)
+            {
+                Console.Write(numToPrint + " ");
+            }
+        }
+    }
+}
