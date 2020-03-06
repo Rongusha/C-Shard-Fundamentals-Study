005342f3c5ffacb92bbcc15bf7647aaf9878752a
Teodor Hristov <teodor214@gmail.com>
2020-03-06 15:48:50 +0200

    Add files via upload

---
+++ 01. Reverse Strings/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Linq;
+using System.Text;
+
+namespace _01._Reverse_Strings
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string command = string.Empty;
+
+            while ((command = Console.ReadLine()) != "end")
+            {
+                string reverced = "";
+                for (int i = command.Length-1; i >= 0; i--)
+                {
+                    reverced += command[i];
+                }
+                Console.WriteLine($"{command} = {reverced}");
+            }
+        }
+    }
+}
---
+++ 01. Valid Usernames/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Linq;
+using System.Text;
+
+namespace _01._Valid_Usernames
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+        }
+    }
+}
