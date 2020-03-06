a442eebdf6aef8aae40cc6352b6f5ef21c6737b4
Teodor Hristov <teodor214@gmail.com>
2020-03-06 16:14:09 +0200

    Add files via upload

---
+++ 08. Company Users/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace _08._Company_Users
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var workers = new Dictionary<string, List<string>>();
+            string command = string.Empty;
+            bool isThere = false;
+            while ((command = Console.ReadLine())!="End")
+            {
+                var tokens = command.Split(" -> ").ToArray();
+                string company = tokens[0];
+                string worker = tokens[1];
+
+                if (!workers.ContainsKey(company))
+                {
+                    workers[company] = new List<string>();
+                    workers[company].Add(worker);
+                }
+                else
+                {
+                    foreach (var item in workers)
+                    {
+                        foreach (var x in item.Value)
+                        {
+                            if (x==worker)
+                            {
+                                isThere = true;
+                                break;
+                            }
+                           
+                        }
+                    }
+                    if (!isThere)
+                    {
+                        workers[company].Add(worker);
+                    }
+                    
+                }
+            }
+
+           
+            foreach (var item in workers.OrderBy(x=>x.Key))
+            {
+                Console.WriteLine(item.Key);
+                foreach (var x in item.Value)
+                {
+                    Console.WriteLine($"-- {x}");
+                }
+            }
+        }
+    }
+}
