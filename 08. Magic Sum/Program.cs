090f6ba3745420e0aa5e721a5797b94f4fb86542
Teodor Hristov <teodor214@gmail.com>
2020-03-06 17:54:17 +0200

    Add files via upload

---
+++ 08. Magic Sum/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Linq;
+
+namespace _08._Magic_Sum
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers = Console.ReadLine()
+                .Split()
+                .Select(int.Parse)
+                .ToArray();
+
+            int sum = int.Parse(Console.ReadLine());
+
+            int currentNum = 0;
+            int newNum = 0;
+            int currentSum = 0;
+
+            for (int i = 0; i < numbers.Length-1; i++)
+            {
+                currentNum = numbers[i];
+                for (int j = i+1; j < numbers.Length; j++)
+                {
+                    newNum = numbers[j];
+                    currentSum = currentNum + newNum;
+
+                    if (currentSum == sum)
+                    {
+                        Console.Write($"{currentNum} {newNum}");
+                        Console.WriteLine();
+                    }
+                }
+            }
+        }
+    }
+}
