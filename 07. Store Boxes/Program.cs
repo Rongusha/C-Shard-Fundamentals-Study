6c7d7525669fe9e80530e06ab9ea53da09bcdcb9
Teodor Hristov <teodor214@gmail.com>
2020-03-06 17:22:10 +0200

    Add files via upload

---
+++ 07. Store Boxes/Program.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace _07._Store_Boxes
+{
+    class Item
+    {
+        public string Name { get; set; }
+        public double Price { get; set; }
+    }
+
+    class Box
+    {
+        public Box()
+        {
+            Item = new Item();
+        }
+
+        public string SerialNum { get; set; }
+        public Item Item { get; set; }
+        public int Quantity { get; set; }
+        public double Price { get; set; }
+
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Box> boxes = new List<Box>();
+
+            string command = string.Empty;
+
+            while ((command = Console.ReadLine()) != "end")
+            {
+                string[] info = command.Split(" ");
+                string serialNum = info[0];
+                string name = info[1];
+                int quantity = int.Parse(info[2]);
+                double itemPrice = double.Parse(info[3]);
+                double boxPrice = itemPrice * quantity;
+
+                Item item = new Item()
+                {
+                    Name = name,
+                    Price = itemPrice
+                };
+
+                Box box = new Box()
+                {
+                    SerialNum = serialNum,
+                    Item = item,
+                    Quantity = quantity,
+                    Price = boxPrice
+
+
+                };
+
+                boxes.Add(box);
+            }
+            
+            IEnumerable<Box> ordered = boxes.OrderBy(Box => Box.Price);
+
+            foreach (Box box in ordered.Reverse())
+            {
+                Console.WriteLine($"{box.SerialNum}\n-- {box.Item.Name} - ${box.Item.Price:f2}: {box.Quantity}\n-- ${box.Price:f2}");
+            }
+        }
+    }
+}
---
+++ 08. Vehicle Catalogue/Program.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace _08._Vehicle_Catalogue
+{
+    class Car
+    {
+        public string Brand { get; set; }
+        public string Model { get; set; }
+        public int Hp { get; set; }
+
+    }
+
+    class Truck
+    {
+        public string Brand { get; set; }
+        public string Model { get; set; }
+        public int Weight { get; set; }
+    }
+
+    class Catalogue
+    {
+        public Catalogue(List<Car> cars, List<Truck> trucks)
+        {
+            this.Cars = cars;
+            this.Trucks = trucks;
+        }
+
+        public List<Car> Cars { get; set; }
+        public List<Truck> Trucks { get; set; }
+
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Car> cars = new List<Car>();
+            List<Truck> trucks = new List<Truck>();
+
+            string command = string.Empty;
+
+            while ((command = Console.ReadLine()) != "end")
+            {
+                string[] info = command.Split("/");
+                string type = info[0];
+                string brand = info[1];
+                string model = info[2];
+                int data = int.Parse(info[3]);
+                if (type == "Truck")
+                {
+                    Truck truck = new Truck()
+                    {
+                        Brand = brand,
+                        Model = model,
+                        Weight = data
+                    };
+
+                    trucks.Add(truck);
+                }
+                else if (type == "Car")
+                {
+                    Car car = new Car()
+                    {
+                        Brand = brand,
+                        Model = model,
+                        Hp = data
+                    };
+
+                    cars.Add(car);
+                }
+            }
+
+            Catalogue catalogue = new Catalogue(cars, trucks);
+
+            IEnumerable<Car> orderedCars = cars.OrderBy(car => car.Brand); 
+
+            if (cars.Count > 0)
+            {
+                Console.WriteLine("Cars:");
+
+                foreach (Car car in orderedCars)
+                {
+                    Console.WriteLine($"{car.Brand}: {car.Model} - {car.Hp}hp");
+                }
+            }
+           
+            IEnumerable<Truck> orderedTrucks = trucks.OrderBy(truck => truck.Brand);
+
+            if (trucks.Count>0)
+            {
+                Console.WriteLine("Trucks:");
+
+                foreach (Truck truck in orderedTrucks)
+                {
+                    Console.WriteLine($"{truck.Brand}: {truck.Model} - {truck.Weight}kg");
+                }
+            }
+        }
+    }
+}
