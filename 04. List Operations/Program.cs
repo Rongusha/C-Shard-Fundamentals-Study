afd6da957431c714a6981b7e5c6c699c4fab9d95
Teodor Hristov <teodor214@gmail.com>
2020-03-06 17:44:22 +0200

    Add files via upload

---
+++ 04. List Operations/Program.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace _04._List_Operations
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> numbers = Console.ReadLine()
+                .Split()
+                .Select(int.Parse)
+                .ToList();
+
+            string command = string.Empty;
+
+            while ((command = Console.ReadLine()) != "End")
+            {
+                string[] words = command.Split();
+                string action = words[0];
+                int num;
+
+                switch (action)
+                {
+                    case "Add":
+                        num = int.Parse(words[1]);
+                        numbers.Add(num);
+                        break;
+                    case "Insert":
+                        num = int.Parse(words[1]);
+                        int indexNum = int.Parse(words[2]);
+
+                        if (indexNum >= numbers.Count || indexNum <0)
+                        {
+                            Console.WriteLine("Invalid index");
+                        }
+                        else
+                        {
+                            numbers.Insert(indexNum, num);
+                        }
+                        break;
+                    case "Remove":
+                        num = int.Parse(words[1]);
+
+                        if (num >= numbers.Count || num<0)
+                        {
+                            Console.WriteLine("Invalid index");
+                        }
+                        else
+                        {
+                            numbers.RemoveAt(num);
+                        }
+                        break;
+                    case "Shift":
+                        int count = int.Parse(words[2]);
+                        ShiftTheList(numbers, count, words[1]);
+                        break;
+                    default:
+                        Console.WriteLine("Invalid index");
+                        break;
+                }
+            }
+
+            Console.WriteLine(string.Join(" ", numbers));
+        }
+
+        static void ShiftTheList(List<int> items, int count, string direction)
+        {
+            int nItem;
+
+            if (direction == "right")
+            {
+                for (int i = 0; i < count; i++)
+                {
+                    nItem = items[items.Count - 1];     // get the last item of the list
+                    items.Remove(nItem);  // remove this item from the end  ...
+                    items.Insert(0, nItem);         // ... and insert this (last) item in front of the list
+                }
+            }
+            else if (direction == "left")
+            {
+                for (int i = 0; i < count; i++)
+                {
+                    nItem = items[0];  //get the first element
+                    items.Insert(items.Count, nItem); //insert it at the back of the list
+                    items.Remove(items[0]);  //remove the element of the list
+                }
+            }
+        }
+    }
+}
---
+++ 05. Bomb Numbers/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace _05._Bomb_Numbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> numbers = Console.ReadLine()
+                .Split()
+                .Select(int.Parse)
+                .ToList();
+
+            List<int> actionNums = Console.ReadLine()
+                .Split()
+                .Select(int.Parse)
+                .ToList();
+
+            int bombNum = actionNums[0];
+            int powerNum = actionNums[1];
+            int bombIndex = numbers.IndexOf(bombNum);
+           
+            while (bombIndex>=0)
+            {
+                for (int i = 0; i < powerNum; i++)
+                {
+                    if (bombIndex < numbers.Count-1)
+                    {
+                        numbers.RemoveAt(bombIndex + 1);
+                    }
+                }
+
+                for (int i = 0; i < powerNum; i++)
+                {
+                    if (bombIndex >0)
+                    {
+                        numbers.RemoveAt(bombIndex - 1);
+                        bombIndex--;
+                    }
+                }
+
+                numbers.RemoveAt(bombIndex);
+                bombIndex = numbers.IndexOf(bombNum);
+            }
+
+            Console.WriteLine(numbers.Sum());
+        }
+    }
+}
